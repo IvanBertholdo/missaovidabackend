85c2ff7fe7be4ce64e66eb2a54ead2354e857123
Ivan Bertholdo <143834262+IvanBertholdo@users.noreply.github.com>
2024-08-22 12:08:22 -0300

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,49 +1,18 @@
 const Fastify = require('fastify');
-const fastifycors = require('@fastify/cors');
-const swagger = require('@fastify/swagger');
-const swaggerui = require('@fastify/swagger-ui');
-const fastifyJwt = require('@fastify/jwt');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const openApiDocs = require('./openapi');
-const strongPassword = require('./passwordUtils');
-const acolhidoRoutes = require('./routes/acolhidoRoutes');
 
 const app = Fastify({ logger: true });
 const PORT = process.env.PORT || 3333;
 
+// Rota GET para "/"
 app.get('/', async (request, reply) => {
-    reply.send({ message: 'Servidor online!' });
-  });
-
-app.register(fastifycors);
-
-app.register(fastifyJwt, {
-    secret: strongPassword
+  reply.send({ message: 'Olá, mundo!' });
 });
 
-// Middleware de autenticação
-app.decorate("authenticate", async (request, reply) => {
-    try {
-        await request.jwtVerify();
-    } catch (err) {
-        reply.send(err);
-    }
-})
-
-app.register(swagger, openApiDocs);
-app.register(swaggerui, openApiDocs);
-
-// Registre suas rotas aqui:
-app.register(acolhidoRoutes, { prefix: '/api' });
-app.register(userRoutes, { prefix: '/api' });
-app.register(authRoutes, { prefix: '/api/auth' });
-
-// Iniciar o servidor 
+// Iniciar o servidor
 app.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
-    if (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-    app.log.info(`Server is running on port ${PORT} at ${address}`); 
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+  app.log.info(`Server is running on port ${PORT} at ${address}`); 
 });
