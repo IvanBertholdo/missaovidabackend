6a7419653828c7297700ff9c5b92b8f317f03912
Ivan Bertholdo <143834262+IvanBertholdo@users.noreply.github.com>
2024-08-22 10:34:50 -0300

    Update index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,6 @@
 const { Sequelize } = require('sequelize');
-const config = require('../config/database');
+//const config = require('../config/database');
+const { DATABASE_URL } = process.env;
 const sequelize = new Sequelize(config);
 
 const db = {};
